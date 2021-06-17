5c892972cd257818d2c488204325b5229f8b915e
Patryk Łubik <patryk.lubik@wp.pl>
2021-06-17 18:53:54 +0200

    basic authorization type

---
+++ src/main/java/pl/patryklubik/springsecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,30 @@
+package pl.patryklubik.springsecurity.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+/**
+ * Create by Patryk Łubik on 17.06.2021.
+ */
+
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
